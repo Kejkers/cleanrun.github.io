b2c958f227d2e17952997718b2eaf0a065a95005
matvey <tod13yung@ya.ru>
2023-09-30 13:51:29 +0400

    feat: initial player control

---
+++ README.md
@@ -0,0 +1,13 @@
+## What is this
+
+It is a game for ludum dare 54
+
+## How to run locally
+
+```
+npm start
+```
+
+## Website
+
+https://kejkers.github.io/cleanrun.github.io
--- game.js
+++ game.js
@@ -1,9 +1,16 @@
-class Example extends Phaser.Scene
+const W = 1280;
+const H = 720;
+
+class BaseScene extends Phaser.Scene
 {
     constructor ()
     {
         super();
     }
+
+    player;
+    cursors;
+    speed;
 
     preload ()
     {
@@ -15,25 +22,18 @@ class Example extends Phaser.Scene
 
     create ()
     {
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.speed = 2;
         this.instantiate_objects();
     }
 
     instantiate_objects() {
-        this.add.image(0, 0, 'bg').setOrigin(0, 0);
+        this.add.image(W / 2, H / 2, 'bg');
 
-        const particles = this.add.particles(0, 0, 'red', {
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        });
+        this.player = this.matter.add.sprite(400, 100, 'pl');
 
-        const logo = this.matter.add.image(400, 100, 'pl');
-
-        logo.setVelocity(0, 0);
-        logo.setBounce(1, 1);
-        // logo.setCollideWorldBounds(true);
-
-        particles.startFollow(logo);
+        this.player.setVelocity(0, 0);
+        this.player.setBounce(1, 1);
 
         const logo2 = this.matter.add.image(400, 400, 'pl');
 
@@ -43,23 +43,49 @@ class Example extends Phaser.Scene
         logo2.setFriction(0.005);
     }
 
+    update ()
+    {
+        this.handle_controls();
+    }
+
+    handle_controls(player) {
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-this.speed);
+            // player.anims.play('left', true);
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(this.speed);
+            // player.anims.play('right', true);
+        } else {
+            this.player.setVelocityX(0);
+            // player.anims.play('turn');
+        }
+
+        if (this.cursors.up.isDown) {
+            this.player.setVelocityY(-this.speed);
+        } else if (this.cursors.down.isDown) {
+            this.player.setVelocityY(this.speed);
+        } else {
+            this.player.setVelocityY(0);
+            // player.anims.play('turn');
+        }
+    }
 }
 
 
 const config = {
     type: Phaser.AUTO,
-    width: 1280,
-    height: 720,
+    width: W,
+    height: H,
     physics: {
         default: 'matter',
         matter: {
             debug: true,
             gravity: {
-                y: 0.3
+                y: 0.0
             }
         }
     },
-    scene: Example
+    scene: BaseScene
 };
 
 const game = new Phaser.Game(config);
