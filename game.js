30c45caa2769f7caffca84b56488127afa858b5f
matvey <tod13yung@ya.ru>
2023-09-30 15:57:27 +0400

    feat: added anims

--- game.js
+++ game.js
@@ -17,6 +17,7 @@ class BaseScene extends Phaser.Scene
         this.load.path = "assets/";
         this.load.image('bg', 'bg/room1.png');
         this.load.image('pl', 'fg/player.png');
+        this.load.atlas('player_sheet', 'fg/player_sheet.png', 'fg/player_sheet.json');
         this.load.image('red', 'fg/red.png');
     }
 
@@ -30,10 +31,25 @@ class BaseScene extends Phaser.Scene
     instantiate_objects() {
         this.add.image(W / 2, H / 2, 'bg');
 
-        this.player = this.matter.add.sprite(400, 100, 'pl');
+        this.player = this.matter.add.sprite(400, 100, 'player_sheet');
 
         this.player.setVelocity(0, 0);
         this.player.setBounce(1, 1);
+        this.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNumbers('player_sheet', { start: 0, end: 0 }),
+            frameRate: 10,
+            duration: 1,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'go',
+            frames: this.anims.generateFrameNumbers('player_sheet', { start: 0, end: 3 }),
+            frameRate: 10,
+            duration: 4,
+            repeat: -1
+        });
+        this.player.play('idle');
 
         const logo2 = this.matter.add.image(400, 400, 'pl');
 
@@ -51,22 +67,22 @@ class BaseScene extends Phaser.Scene
     handle_controls(player) {
         if (this.cursors.left.isDown) {
             this.player.setVelocityX(-this.speed);
-            // player.anims.play('left', true);
+            this.player.anims.play('go', true);
         } else if (this.cursors.right.isDown) {
             this.player.setVelocityX(this.speed);
-            // player.anims.play('right', true);
+            this.player.anims.play('go', true);
         } else {
             this.player.setVelocityX(0);
-            // player.anims.play('turn');
         }
 
         if (this.cursors.up.isDown) {
             this.player.setVelocityY(-this.speed);
+            this.player.anims.play('go', true);
         } else if (this.cursors.down.isDown) {
             this.player.setVelocityY(this.speed);
+            this.player.anims.play('go', true);
         } else {
             this.player.setVelocityY(0);
-            // player.anims.play('turn');
         }
     }
 }
