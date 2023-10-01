52803e51931ba14bcca4598fe9b389aa30e3863b
matvey <tod13yung@ya.ru>
2023-10-01 15:36:57 +0400

    feat: new bg, added window and win condition

--- game.js
+++ game.js
@@ -1,12 +1,15 @@
 const W = 1280;
 const H = 720;
 
+
 function randomPos() {
     return {
         x: Math.random(),
         y: Math.random()
     }
 }
+
+const maxTrash = 10;
 
 class BaseScene extends Phaser.Scene {
 
@@ -23,6 +26,7 @@ class BaseScene extends Phaser.Scene {
     rubbish;
     timeSeconds;
     timeText;
+    trashLeft;
 
     preload() {
         this.load.path = "assets/";
@@ -30,8 +34,10 @@ class BaseScene extends Phaser.Scene {
         this.load.image('pl', 'fg/player.png');
         this.load.atlas('player_sheet', 'fg/player_sheet.png', 'fg/player_sheet.json');
         this.load.image('redwall', 'fg/redwall.png');
-        this.load.image('trash', 'fg/trash.png');
-        this.load.image('dildo', 'fg/dildo.png');
+        this.load.image('socks', 'fg/socks.png');
+        this.load.image('transparent', 'fg/transparent.png');
+        this.load.image('window', 'fg/window.png');
+        this.load.image('table', 'fg/table.png');
 
         this.itemPickAndDrop.preloadAssets();
     }
@@ -43,15 +49,34 @@ class BaseScene extends Phaser.Scene {
         this.instantiate_objects();
         this.timeSeconds = 90;
         this.timeText = this.add.text(W / 2, 8, 'Timer felt asleep :<', { color: '#ffffff' });
+        this.add.text(50, 8, 'Press SPACE to restart', { color: '#ffffff' });
     }
 
     instantiate_objects() {
+        this.trashLeft = maxTrash;
+
         this.add.image(W / 2, H / 2, 'bg');
+
+        const wallwindow = this.matter.add.image(W / 2, 140, 'window').setScale(2, 2);
+
+        wallwindow.setSensor(true);
+        wallwindow.setStatic(true);
+        wallwindow.setOnCollide(function(coldata) {
+            if (!coldata.bodyA.isStatic && !(coldata.bodyA.gameObject.name === "player")) {
+                this.gameObject.scene.trashLeft -= 1;
+                coldata.bodyA.gameObject.destroy();
+
+            } else if (!coldata.bodyB.isStatic && !(coldata.bodyB.gameObject.name === "player")) {
+                this.gameObject.scene.trashLeft -= 1;
+                coldata.bodyB.gameObject.destroy();
+            }
+        });
 
         this.player = this.matter.add.sprite(100, 400, 'player_sheet');
 
         this.player.setVelocity(0, 0);
         this.player.setBounce(1, 1);
+        this.player.name = "player";
         this.anims.create({
             key: 'idle',
             frames: this.anims.generateFrameNumbers('player_sheet', { start: 0, end: 0 }),
@@ -68,23 +93,40 @@ class BaseScene extends Phaser.Scene {
         });
         this.player.play('idle');
 
-        const trash = this.matter.add.image(400, 500, 'trash');
-
-        trash.setBounce(1, 1);
-        trash.setStatic(true);
-        trash.setFriction(0.005);
-
-        const topwall = this.matter.add.image(0, 0, 'redwall').setScale(30, 6);
+        const topwall = this.matter.add.image(0, 0, 'transparent').setScale(1280, 190);
 
         topwall.setBounce(1, 1);
         topwall.setStatic(true);
         topwall.setFriction(0.005);
 
-        this.itemPickAndDrop.setPlayer(this.player);
+        const leftwall = this.matter.add.image(0, 0, 'transparent').setScale(1, 720);
+
+        leftwall.setBounce(1, 1);
+        leftwall.setStatic(true);
+        leftwall.setFriction(0.005);
+
+        const rightwall = this.matter.add.image(1278, 0, 'transparent').setScale(1, 720);
+
+        rightwall.setBounce(1, 1);
+        rightwall.setStatic(true);
+        rightwall.setFriction(0.005);
+
+        const botwall = this.matter.add.image(0, 718, 'transparent').setScale(1280, 1);
+
+        botwall.setBounce(1, 1);
+        botwall.setStatic(true);
+        botwall.setFriction(0.005);
+
         this.itemPickAndDrop.initItemPlaceholder();
 
-        for (let i = 0; i < 50; i++) {
-            this.createRubbish(randomPos(), 'dildo');
+        const table = this.matter.add.image(W/2, H/2, 'table');
+
+        table.setBounce(1, 1);
+        table.setStatic(true);
+        table.setFriction(0.005);
+
+        for (let i = 0; i < maxTrash; i++) {
+            this.createRubbish(randomPos(), 'socks');
         }
     }
 
@@ -102,14 +144,21 @@ class BaseScene extends Phaser.Scene {
             this.scene.start('pre');
         }
 
-        const tdiff = this.timeSeconds - (this.time.now - this.time.startTime) / 1000;
-        if (tdiff <= 0) {
+        if (this.trashLeft === 0) {
+            this.timeText.text = "You've done this!";
             return;
         }
+
+        const tdiff = this.timeSeconds - (this.time.now - this.time.startTime) / 1000;
+        if (tdiff <= 0 || this.player.body === undefined) {
+            return;
+        }
+
 
         this.player.rotation = 0;
         this.handle_controls();
         this.upd_time(tdiff);
+
     }
 
     handle_controls(player) {
@@ -147,7 +196,7 @@ class BaseScene extends Phaser.Scene {
 
     upd_time(diff) {
         let secondsLeft = Math.round(diff);
-        let minutesLeft = (secondsLeft >= 60)? Math.round(secondsLeft / 60) : 0;
+        let minutesLeft = (secondsLeft >= 60)? Math.floor(secondsLeft / 60) : 0;
         let residSeconds = secondsLeft - minutesLeft * 60;
         this.timeText.text = minutesLeft.toString() + ':' + ((residSeconds < 10)? '0' : '') + residSeconds.toString();
     }
@@ -173,6 +222,7 @@ class Menu extends Phaser.Scene {
     }
 
     start_game() {
+        // currentScene = 'learn';
         currentScene = 'basescene';
         this.scene.scene.start(currentScene);
     }
@@ -192,6 +242,192 @@ class PreLoader extends Phaser.Scene {
 }
 
 
+// TODO: simplify
+// TODO: simplify
+// TODO: simplify
+class Learn extends Phaser.Scene {
+    itemPickAndDrop;
+
+    constructor() {
+        super({ key: 'learn' });
+        this.itemPickAndDrop = new ItemPickAndDrop(this, W, H);
+    }
+
+    player;
+    cursors;
+    speed;
+    rubbish;
+    timeSeconds;
+    timeText;
+    trashLeft;
+
+    preload() {
+        this.load.path = "assets/";
+        this.load.image('bg', 'bg/room1.png');
+        this.load.image('pl', 'fg/player.png');
+        this.load.atlas('player_sheet', 'fg/player_sheet.png', 'fg/player_sheet.json');
+        this.load.image('redwall', 'fg/redwall.png');
+        this.load.image('socks', 'fg/socks.png');
+        this.load.image('transparent', 'fg/transparent.png');
+        this.load.image('window', 'fg/window.png');
+
+        this.itemPickAndDrop.preloadAssets();
+    }
+
+    create() {
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.speed = 6;
+        this.itemPickAndDrop.initKeyboardAction();
+        this.instantiate_objects();
+        this.timeText = this.add.text(50, 8, 'Your parents are coming! Move to socks and click on it, then throw it to window.', { color: '#ffffff' });
+    }
+
+    instantiate_objects() {
+        this.trashLeft = 2;
+
+        this.add.image(W / 2, H / 2, 'bg');
+
+        const wallwindow = this.matter.add.image(W / 2, 360, 'window').setScale(2, 2);
+
+        wallwindow.setSensor(true);
+        wallwindow.setStatic(true);
+        wallwindow.setOnCollide(function(coldata) {
+            if (!coldata.bodyA.isStatic && !(coldata.bodyA.gameObject.name === "player")) {
+                this.gameObject.scene.trashLeft -= 1;
+                console.log(this.trashLeft);
+                coldata.bodyA.gameObject.destroy();
+
+            } else if (!coldata.bodyB.isStatic && !(coldata.bodyB.gameObject.name === "player")) {
+                this.gameObject.scene.trashLeft -= 1;
+                console.log(this.trashLeft);
+                coldata.bodyB.gameObject.destroy();
+            }
+        });
+
+        this.player = this.matter.add.sprite(100, 400, 'player_sheet');
+
+        this.player.setVelocity(0, 0);
+        this.player.setBounce(1, 1);
+        this.player.name = "player";
+        this.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNumbers('player_sheet', { start: 0, end: 0 }),
+            frameRate: 10,
+            duration: 1,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'go',
+            frames: this.anims.generateFrameNumbers('player_sheet', { start: 0, end: 3 }),
+            frameRate: 10,
+            duration: 4,
+            repeat: -1
+        });
+        this.player.play('idle');
+
+        const topwall = this.matter.add.image(0, 0, 'transparent').setScale(1280, 190);
+
+        topwall.setBounce(1, 1);
+        topwall.setStatic(true);
+        topwall.setFriction(0.005);
+
+        const leftwall = this.matter.add.image(0, 0, 'transparent').setScale(1, 720);
+
+        leftwall.setBounce(1, 1);
+        leftwall.setStatic(true);
+        leftwall.setFriction(0.005);
+
+        const rightwall = this.matter.add.image(1278, 0, 'transparent').setScale(1, 720);
+
+        rightwall.setBounce(1, 1);
+        rightwall.setStatic(true);
+        rightwall.setFriction(0.005);
+
+        const botwall = this.matter.add.image(0, 718, 'transparent').setScale(1280, 1);
+
+        botwall.setBounce(1, 1);
+        botwall.setStatic(true);
+        botwall.setFriction(0.005);
+
+        this.itemPickAndDrop.initItemPlaceholder();
+
+        for (let i = 0; i < 2; i++) {
+            this.createRubbish(randomPos(), 'socks');
+        }
+    }
+
+    createRubbish(xy, imageName) {
+        const r = this.matter.add.image(
+            W * xy.x,
+            H * xy.y,
+            imageName
+        );
+        r.setInteractive(new Phaser.Geom.Rectangle(-8, -8, 48, 48), Phaser.Geom.Rectangle.Contains);
+    }
+
+    update() {
+        if (this.cursors.space.isDown) {
+            this.scene.start('pre');
+        }
+
+        if (this.trashLeft === 0) {
+            this.input.keyboard.on('keydown', this.next_scene);
+            this.timeText.text = "You are ready for the real challenge! Press any key...";
+            return;
+        }
+
+        const tdiff = this.timeSeconds - (this.time.now - this.time.startTime) / 1000;
+        if (tdiff <= 0 || this.player.body === undefined) {
+            return;
+        }
+
+        this.player.rotation = 0;
+        this.handle_controls();
+
+    }
+
+    handle_controls(player) {
+        let speed = (this.cursors.shift.isDown) ? this.speed * 2 : this.speed;
+
+        let xmove = this.cursors.left.isDown || this.cursors.right.isDown;
+        let ymove = this.cursors.up.isDown || this.cursors.down.isDown;
+
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX((ymove)? -speed / 1.5 : -speed);
+            this.player.anims.play('go', true);
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX((ymove)? speed / 1.5 : speed);
+            this.player.anims.play('go', true);
+        } else {
+            this.player.setVelocityX(0);
+        }
+
+        if (this.cursors.up.isDown) {
+            this.player.setVelocityY((xmove)? -speed / 1.5 : -speed);
+            this.player.anims.play('go', true);
+        } else if (this.cursors.down.isDown) {
+            this.player.setVelocityY((xmove)? speed / 1.5 : speed);
+            this.player.anims.play('go', true);
+        } else {
+            this.player.setVelocityY(0);
+        }
+
+        if (!xmove && !ymove) {
+            this.player.anims.play('idle', true);
+        }
+
+        this.itemPickAndDrop.checkIfKeyboardActionHappens();
+    }
+
+    next_scene() {
+        currentScene = 'basescene';
+        this.scene.scene.start(currentScene);
+    }
+}
+// TODO: simplify
+// TODO: simplify
+// TODO: simplify
+
 
 const config = {
     type: Phaser.AUTO,
@@ -206,7 +442,7 @@ const config = {
             }
         }
     },
-    scene: [PreLoader, Menu, BaseScene]
+    scene: [PreLoader, Menu, Learn, BaseScene]
 };
 
 const game = new Phaser.Game(config);
@@ -218,10 +454,9 @@ class ItemPickAndDrop {
     itemOnPlaceholder;
     squarePos;
     canThrow; // ебаный костыль
-    player;
 
     static THROW_DELTA_POS = 100;
-    static THROW_SPEED = 10;
+    static THROW_SPEED = 15;
     static SQUARE_POS_X = 0.9;
     static SQUARE_POS_Y = 0.05;
 
@@ -237,10 +472,6 @@ class ItemPickAndDrop {
         this.game.load.image('square', 'fg/square.png');
     }
 
-    setPlayer(player) {
-        this.player = player;
-    }
-
     initItemPlaceholder() {
         let {x, y} = this.squarePos;
         this.game.add.image(x, y, 'square');
@@ -254,7 +485,7 @@ class ItemPickAndDrop {
                 return;
             }
             if (!this.isPickedUp()) {
-                this.take(gameObject, this.player.x, this.player.y);
+                this.take(gameObject);
             }
         });
     }
@@ -272,9 +503,9 @@ class ItemPickAndDrop {
         }
     }
 
-    take(item, playerX, playerY) {
-        const distance = Math.sqrt((playerX - item.x) * (playerX - item.x) + (playerY - item.y) * (playerY - item.y));
-        if (distance > 100) {
+    take(item) {
+        const distance = Math.sqrt((this.game.player.x - item.x) * (this.game.player.x - item.x) + (this.game.player.y - item.y) * (this.game.player.y - item.y));
+        if (distance > 160) {
             return;
         }
         let {x, y} = this.squarePos;
@@ -300,6 +531,7 @@ class ItemPickAndDrop {
         item.y = item.y + (yFactor * ItemPickAndDrop.THROW_DELTA_POS);
         item.setVelocityX(xFactor * ItemPickAndDrop.THROW_SPEED);
         item.setVelocityY(yFactor * ItemPickAndDrop.THROW_SPEED);
+        item.setAngularVelocity(0.25);
     }
 
     isPickedUp() {
